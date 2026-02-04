a7056a46ac92ed07f0a7ac9d9728a5b158b536de
Dipannita Sharma <dipannitasharma1@gmail.com>
2026-02-04 23:41:57 +0530

    updated main

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-
 import { BrowserRouter } from "react-router-dom";
-
 import { Buffer } from "buffer";
 window.Buffer = Buffer;
-
 import App from "./App";
 import "./index.css";
 
